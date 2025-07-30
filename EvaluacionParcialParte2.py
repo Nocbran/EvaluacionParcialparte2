553386ce6fe2d8ec562618a4392477bd6a4321fe
Brand <vasbran111@gmail.com>
2025-07-30 18:57:11 -0500

    .

---
+++ EvaluacionParcialParte2.py
@@ -0,0 +1,53 @@
+def CalcMenu(n):
+    print("CALCULO DE MCD")
+    a = int(input("Ingrese el primer numero: "))
+    b = int(input("Ingrese el segundo numero: "))
+    a, b == cmd
+
+def CadenaRepetida(r):
+    if n == 3:
+        return n
+    else
+        return  n * n
+
+def ContarLetras(l,palabra):
+    print("Bienvenido al conteo de letra: ")
+    palabra = input("Ingrese una palabra: ")
+    letra = input("Ingrese la letra que desea ver cuantas veses se repite: ")
+
+
+def CalcDigitosNnumero(d):
+    d = int(input("ingres"))
+
+def menu():
+    print("*************MENU*************")
+    print("1. Calcular el MCD de dos numeros")
+    print("2. Cadena repetida N veces")
+    print("3. Contar cuantas veces aparece una letra en una cadena")
+    print("4. Convertir un numero decimal a binario")
+    print("5. Calcular cuantos digitos tiene un numero")
+
+
+def ejecutar_opcion(opcion):
+    match opcion:
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        case 5:
+
+        case _:
+
+def main():
+    opcion = 0
+    while opcion != 5:
+        menu()
+        try:
+            opcion = int(input("Elige una opción (1-5): "))
+            ejecutar_opcion(opcion)
+        except ValueError:
+            print("Por favor ingresa un número válido.")
